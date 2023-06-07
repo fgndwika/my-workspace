54606cb65f440b07b4099e90cd73d53558724f65
Fransiskus Gst. Ngr. Dwika S <fransiskus.g.n.dwika@gdplabs.id>
2023-06-08 00:30:17 +0700

    add user model and service

---
+++ projects/libs/entity/user/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+import { User } from './user.model';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  readonly storageKey: string = 'USERS';
+
+  getUsers(): User[] {
+    const users = localStorage.getItem(this.storageKey);
+    return users ? JSON.parse(users) : [];
+  }
+
+  addUser(user: User): void {
+    const users = this.getUsers();
+    users.push(user);
+    localStorage.setItem(this.storageKey, JSON.stringify(users));
+  }
+
+  updateUser(user: User): void {
+    const users = this.getUsers();
+    const index = users.findIndex(u => u.id === user.id);
+    users[index] = user;
+    localStorage.setItem(this.storageKey, JSON.stringify(users));
+  }
+
+  deleteUser(id: string): void {
+    const users = this.getUsers();
+    const index = users.findIndex(u => u.id === id);
+    users.splice(index, 1);
+    localStorage.setItem(this.storageKey, JSON.stringify(users));
+  }
+
+  getUser(id: string): User {
+    const users = this.getUsers();
+    return users.find(u => u.id === id);
+  }
+}
