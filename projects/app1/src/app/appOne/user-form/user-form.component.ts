e1f66f0a12ec6cefd67691886daa4b89b569947e
Dwika <fransiskus.g.n.dwika@gdplabs.id>
2023-06-08 10:52:52 +0700

    change ~libs into libs

--- projects/app1/src/app/appOne/user-form/user-form.component.ts
+++ projects/app1/src/app/appOne/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { User, UserService } from '~libs';
+import { User, UserService } from 'libs';
 
 @Component({
   selector: 'app-user-form',
